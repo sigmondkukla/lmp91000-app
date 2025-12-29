13afa0f2e0d76d89c94537f481e4aae444b691dc
Sigmond Kukla <sigkukla@gmail.com>
2025-12-29 03:18:59 -0500

    Remove safeareaview

--- potentiostat/app/_layout.tsx
+++ potentiostat/app/_layout.tsx
@@ -1,6 +1,5 @@
 import { Stack } from 'expo-router';
 import { PaperProvider } from 'react-native-paper';
-import { SafeAreaView } from 'react-native-safe-area-context';
 import PaperTheme from '@/components/PaperTheme'; // Use your existing file
 import { BleProvider } from '@/contexts/BleContext'; // New Context
 
@@ -8,15 +7,12 @@ export default function Layout() {
   const paperTheme = PaperTheme();
 
   return (
-    <PaperProvider theme={paperTheme}>
-      <BleProvider>
-        <SafeAreaView style={{ flex: 1, backgroundColor: paperTheme.colors.background }}>
-           {/* Stack points to (tabs) folder as the initial route */}
-          <Stack screenOptions={{ headerShown: false }}>
-            <Stack.Screen name="(tabs)" />
-          </Stack>
-        </SafeAreaView>
-      </BleProvider>
-    </PaperProvider>
+    <BleProvider>
+      <PaperProvider theme={paperTheme}>
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="(tabs)" />
+        </Stack>
+      </PaperProvider>
+    </BleProvider>
   );
 }
